12ca001da3154245a46831fbf8bc87b8549c220e
balda38 <alduin2012@gmail.com>
2017-07-30 19:47:00 +0300

    Update calcFactory.js

--- js/factories/calcFactory.js
+++ js/factories/calcFactory.js
@@ -5,19 +5,13 @@ define(function(){
 		
 		return function(){
 			return new function(){
-<<<<<<< HEAD
+				
 				var output = "0";
 				var buffer = "0";
 				var lastNumber = "0";
 				var lastOperation = null;
 				var operationInfo = null;
-=======
-				this.output = "0";
-				this.buffer = "0";
-				this.lastNumber = "0";
-				this.lastOperation = null;
-				this.operationInfo = null;
->>>>>>> origin/dev
+				
 				var onUpdate = function(){};
 				
 				this.setOnUpdate = function(cb){
@@ -25,11 +19,7 @@ define(function(){
 				};								
 								
 				this.initData = function(){
-<<<<<<< HEAD
 					setData();
-=======
-					this.setData();
->>>>>>> origin/dev
 				};
 				
 				this.updateOutput = function(number){			
@@ -42,11 +32,7 @@ define(function(){
 						output += String(number);
 						buffer += String(number);
 					}
-<<<<<<< HEAD
 					setData();
-=======
-					this.setData();
->>>>>>> origin/dev
 				};
 				
 				this.getOperation = function(operation){
@@ -71,15 +57,9 @@ define(function(){
 						default:
 							break;
 					}
-<<<<<<< HEAD
 					lastNumber = parseInt(buffer, 10);
 					buffer = "0";
 					setData();
-=======
-					this.lastNumber = parseInt(this.buffer, 10);
-					this.buffer = "0";
-					this.setData();
->>>>>>> origin/dev
 				};
 				
 				this.equality = function(){
@@ -108,7 +88,6 @@ define(function(){
 						default:
 							break;
 					}
-<<<<<<< HEAD
 					lastOperation = "=";
 					setData();
 				};
@@ -119,18 +98,14 @@ define(function(){
 					lastNumber = null;
 					lastOperation = null;
 					setData();
-=======
-					this.lastOperation = "=";
-					this.setData();
 				};
 				
 				this.resetAll = function(){
-					this.output = "0";
-					this.buffer = "0";
-					this.lastNumber = null;
-					this.lastOperation = null;
-					this.setData();
->>>>>>> origin/dev
+					output = "0";
+					buffer = "0";
+					lastNumber = null;
+					lastOperation = null;
+					setData();
 				};
 
 				this.getRandomOperation = function(){
@@ -141,21 +116,13 @@ define(function(){
 					buffer = Math.floor(Math.random() * 1000);
 					operationInfo = lastOperation + buffer;
 					this.equality();
-<<<<<<< HEAD
 					setData();
 				};
 				
 				function setData(){
 					var array = [output, lastNumber, operationInfo]
-=======
-					this.setData();
-				};
-				
-				this.setData = function(){
-					var array = [this.output, this.lastNumber, this.operationInfo]
->>>>>>> origin/dev
-					onUpdate(array);			
-				};
+					onUpdate(array);
+				};				
 			};
 		};
 	}]);
